24071c2ffa4876b1a3a296da29a34f5c2fb09a32
Connor Stroomberg <c.stroomberg@gmail.com>
2015-02-06 17:07:53 +0100

    refactor out login to seperate module

--- src/test/nightwatch/loginTest.js
+++ src/test/nightwatch/loginTest.js
@@ -1,16 +1,10 @@
+var login = require('./utils/login.js');
+
 module.exports = {
   "Trialverse login test" : function (browser) {
-    browser
-      .url("https://tv-test.drugis.org")
-      .waitForElementVisible('body', 1000)
-      .click('button[type="submit"]')
-      .pause(1000)
-
-      .setValue('input[type=email]', ' addistestuser1@gmail.com')
-      .setValue('input[type=password]', 'addistestuser123')
-      .click('input[type="submit"]')
-      .waitForElementVisible('.main-content', 3000)
-      .pause(1000)
+    login(browser, 'https://tv-test.drugis.org')
+      .waitForElementVisible('body', 3000)
+      .pause(2000)
       .assert.containsText('h1', 'trialverse.org')
       .end();
   }

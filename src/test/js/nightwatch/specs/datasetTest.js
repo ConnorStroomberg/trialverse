bbc7a3fa177bf51fcea58f400e2c9d77b3345d6a
Connor Stroomberg <c.stroomberg@gmail.com>
2015-09-18 14:58:21 +0200

    test de study page

--- src/test/js/nightwatch/specs/datasetTest.js
+++ src/test/js/nightwatch/specs/datasetTest.js
@@ -7,15 +7,23 @@ var datasetUid = '3d1b7930-3739-4c82-a52b-106b416c61e9';
 var version = '41be7add-e9ce-4e70-8ae1-3ee96061c6ec';
 var datasetUrl = protocol + host + '/#/users/'+ userHash +'/datasets/' + datasetUid + '/versions/' + version;
 
+// selectors 
+var datasetHeader = 'body > div.ng-scope > div > div > div.row.nav-bar-spacer > div > h1';
+var studyTableStudyTitle = 'body > div.ng-scope > div > div > div:nth-child(3) > div > table > tbody > tr > td:nth-child(1) > a';
+var studyNavbarLabel = 'body > div.ng-scope > div > div > div > navbar-directive > div > nav > section > ng-transclude > ul > li.name > h1 > a';
+
 // test the dataset page
 module.exports = {
-  "Trialverse dataset test" : function (browser) {
+  "Trialverse dataset readonly walkthough test" : function (browser) {
     login(browser, process.env.TRIALVERSE_NIGHTWATCH_URL)
       .pause(1000)
       .url(datasetUrl)
       .pause(3000)
-      .assert.containsText('body > div.ng-scope > div > div > div.row.nav-bar-spacer > div > h1', 'Demo dataset')
-      .assert.containsText('body > div.ng-scope > div > div > div:nth-child(3) > div > table > tbody > tr > td:nth-child(1) > a', 'demo study 1')
+      .assert.containsText(datasetHeader, 'Demo dataset')
+      .assert.containsText(studyTableStudyTitle, 'demo study 1')
+      .click(studyTableStudyTitle)
+      .pause(3000)
+      .assert.containsText(studyNavbarLabel, 'Demo dataset / demo study 1')
       .end();
   }
 };
